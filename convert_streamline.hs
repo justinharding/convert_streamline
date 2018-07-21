571f3e40c43fed667e5df34f3c461afa36b84cf8
Justin Harding <justin.harding@midurth.com>
2018-07-21 17:38:37 +1200

    seems to work with dictionary

--- convert_streamline.hs
+++ convert_streamline.hs
@@ -11,7 +11,21 @@ main = do
   -- mapM_ (printTransaction decorateLine decorateTransaction) transactions
   mapM_ (printTransaction "" (return ())) transactions
   let newTransactions = map (modifyTransaction "MITRE 10" "Expenses:Misc" "Expenses:Hardware") transactions
-  mapM_ (printTransaction "" (return ())) newTransactions
+  -- mapM_ (printTransaction "" (return ())) newTransactions
+
+  let dictionary = [("MITRE 10", "Expenses:Misc", "Expenses:Hardware"), ("TOKEN FEE", "Expenses:Misc", "Expenses:BankFee")]
+  let newTransactions2 = map (doIt dictionary) transactions
+  mapM_ (printTransaction "" (return ())) newTransactions2
+
+doIt :: [(String, String, String)] -> [String] -> [String]
+doIt dictionary transaction = foldr (\x y -> modifyTransactionWithTuple x transaction) transaction (filter (check transaction) dictionary)
+
+modifyTransactionWithTuple :: (String, String, String) -> [String] -> [String]
+modifyTransactionWithTuple tuple l = modifyTransaction (fst3 tuple) (snd3 tuple) (thd3 tuple) l
+
+-- getTuple :: [(String, String, String)] -> (String, String, String)
+check :: [String] -> (String, String, String) -> Bool
+check x y = isInfixOf (fst3 y) (head x)
 
 splitIntoTransactions :: (a -> Bool) -> [a] -> [[a]]
 splitIntoTransactions predicate [] = []
@@ -43,6 +57,15 @@ modifyCategory searchString replaceString (x:xs)
 isFirstLineOfTransaction :: String -> Bool
 isFirstLineOfTransaction [] = False
 isFirstLineOfTransaction (x:xs) = not $ elem x " ;"
+
+fst3 :: (a, b, c) -> a
+fst3 (x, _, _) = x
+
+snd3 :: (a, b, c) -> b
+snd3 (_, x, _) = x
+
+thd3 :: (a, b, c) -> c
+thd3 (_, _, x) = x
 
 decorateLine = "%%%"
 
